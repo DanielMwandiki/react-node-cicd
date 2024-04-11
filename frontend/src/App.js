55ead669a05ea26ae78b50cb06b08bc8e66de096
Daniel Mwandiki <danmasketcha@gmail.com>
2024-04-11 15:03:32 +0300

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    getNames();
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUserName(response.data)
+  }
+
+  return (
+    <>
+      <h1>My Frontend</h1>
+      <h3>My name is {userName}</h3>
+    </>
+
+  );
+}
+
+export default App;

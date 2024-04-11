41471838ec2e27857c51fc60452c4ae7b2251f44
Daniel Mwandiki <danmasketcha@gmail.com>
2024-04-11 19:52:29 +0300

    Adding workflow with pm2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => {
-    res.send("Daniel");
+    res.send("Daniel Admin");
 })
 
 app.listen(5000, () => {
